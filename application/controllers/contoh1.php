7239b6a1097037c4da89fbca3977c12e0e34e8e4
AlFadlin <fadlidompu85@gmail.com>
2023-09-25 16:39:19 +0700

    tambah data

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class contoh1 extends CI_Controller
+{
+   public function Index() 
+}
+
+    echo "<h1> Perkenalkan</h1>";
+    echo "Nama saya Fadli saya tinggal di daerah ambarbinangun olaraga yang saya sukai"
